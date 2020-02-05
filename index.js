33e5ed94a34c7a081c5ebb212662fbee593d1b24
Gary Anderson <ganderson@seattleacademy.org>
2020-02-05 09:33:19 -0800

    Add server pages

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const app = express()
 const port = 1500
 let counter = 0
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => res.send('Hello Gary!'))
 
 app.get('/counter', function (req, res) {
 	counter = counter + 1;
   res.send("counter " + counter);
+})
+
+app.get('/date', function (req, res) {
+	let d = new Date();
+	console.log(d);
+  res.send("Date " + d);
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
