1dd96007e1d034cba18233a75ad81c9a1a1065b4
Gary Anderson <ganderson@seattleacademy.org>
2020-02-05 09:14:48 -0800

    Add counter to server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world!")
+const express = require('express')
+const app = express()
+const port = 1500
+let counter = 0
+
+app.get('/', (req, res) => res.send('Hello Gary!'))
+
+app.get('/counter', function (req, res) {
+	counter = counter + 1;
+  res.send("counter " + counter);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
